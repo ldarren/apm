3010eac36802e5955ae8944137c14e61170c2c0e
Darren Liew <ldarren@gmail.com>
2021-09-06 09:18:11 +0800

    added reflow

--- route.js
+++ route.js
@@ -10,13 +10,6 @@ function drawMW(ctx, name, i){
 
 	const mw  = new MW(host, name, {x: 0, y: (i * o.height), width: o.width, height: o.height})
 	ctx.mws.push(mw)
-	const {y, height: h} = Vec(mw.ele).attr(parseInt)('y', 'height').out
-
-	host.setAttribute('width', o.width)
-	host.setAttribute('height', ctx.mws.length * o.height)
-	const hhost = host.ownerSVGElement
-	hhost.setAttribute('height', (o.border * 3) + o.header + (ctx.mws.length * o.height))
-	hhost.setAttribute('width', (o.border * 2) + o.width)
 
 	return ctx
 }
@@ -27,9 +20,26 @@ function Route(host, name, opt, names){
 
 	this.mws = []
 	names.reduce(drawMW, this)
+	this.reflow()
 }
 
 Route.prototype = {
+	reflow(){
+		const o = this.opt
+		const host = this.inner
+		const mws = this.mws
+		const len = this.mws.length
+
+		host.setAttribute('width', o.width)
+		host.setAttribute('height', len * o.height)
+		const hhost = host.ownerSVGElement
+		hhost.setAttribute('height', (o.border * 3) + o.header + (len * o.height))
+		hhost.setAttribute('width', (o.border * 2) + o.width)
+
+		mws.forEach((mw, i) => {
+			Vec(mw.ele).addAttr({x: 0, y: o.height * i})
+		})
+	},
 	onDrag(target){
 		const found = this.mws.find(mw => mw.ele == target)
 		if (!found) return target
@@ -38,6 +48,7 @@ Route.prototype = {
 
 		const {x, y, ele: root} = Vec(found.ele).pos('root').out
 		const o = Vec(found.ele).attr()('width', 'height').rm().out
+		this.reflow()
 
 		const mw = new MW(root, found.name, {x, y, width: o.width, height: o.height})
 		return mw.ele
@@ -46,6 +57,7 @@ Route.prototype = {
 		const name = target.textContent
 		drawMW(this, name, this.mws.length)
 		target.ownerSVGElement.removeChild(target)
+		this.reflow()
 	},
 }
 
