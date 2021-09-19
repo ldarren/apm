e32f60615df99d61f79ed3d2695ac2f5238f3877
Darren Liew <ldarren@gmail.com>
2021-09-19 08:30:50 +0800

    Add Spec Panel (#3)
    * aded button class
    
    * added evt listener
    
    * added create empty route
    
    * added basic saving
    
    * individual mod
    
    * added spec panel

--- param.js
+++ param.js
@@ -2,22 +2,23 @@ const Vec = require('~/vec')
 
 const DEF_OPT = {width: 80, height: 50}
 
-function MW(host, name, opt){
+function Param(host, name, value, opt){
 	const o = Object.assign({}, DEF_OPT, opt)
 
 	this.host = host
 	this.name = name
+	this.value = value
 	this.ele = Vec(host).draw('svg', o).addCl('draggable')
-		.draw('rect', {x:0, y:0, width:'100%', height:'100%'}).style({fill:'#a00', stroke:'#baa'}).addCl('mw')
-		.host().draw('title').text(name)
+		.draw('rect', {x:0, y:0, width:'100%', height:'100%'}).style({fill:'#0aa', stroke:'#aab'}).addCl('param')
+		.host().draw('title').text('@.'+name)
 		.host().draw('text', {x: 10, y:20}).style({fill:'#999', stroke:'#000'}).text(name)
 		.host().ele
 }
 
-MW.prototype = {
+Param.prototype = {
 	save(){
-		return [this.name]
+		return [this.name, this.value]
 	}
 }
 
-return MW
+return Param
