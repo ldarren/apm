41c071628a24320ea1376e26e0d4a1f43c6cff13
Darren Liew <ldarren@gmail.com>
2021-10-09 08:46:56 +0800

    parsing nodejs mod

--- ast.js
+++ ast.js
@@ -1,42 +1,58 @@
-const MAP = {
-	exp: 'ExpressionStatement',
-	func: 'FunctionDeclaration',
-	assign: 'AssignmentExpression',
-}
-
-function find(type, cmp){
-	const pts = this.pt
-	const ltype = MAP[type]A
-	const isCmp = Array.isArray(cmp)
-
-	this.pt = pts.find(pt => {
-		if (pt.type === ltype) {
-			if (isCmp){
-			}else{
-			}
+const MAP_CMP = {
+	exp(pt){
+		if ('ExpressionStatement' === pt.type) return pt.expression
+	},
+	'=': function(pt, cmp){
+		if ('AssignmentExpression' !== pt.type || '=' !== pt.operator) return
+		let left = pt.left
+		if (!MAP_CMP['member'](left, cmp)) return
+		return pt.right
+	},
+	obj(pt){
+		if ('ObjectExpression' === pt.type) return pt.properties
+	},
+	prop(pt, cmp){
+		if ('Property' !== pt.type) return
+		if (Array.isArray(cmp) && pt.key.name !== cmp[0]) return
+		return pt.value
+	},
+	func(pt, cmp){
+		if ('FunctionExpression' !== pt.type) return
+		if (Array.isArray(cmp) && pt.id !== cmp[0]) return
+		return pt.body.body
+	},
+	ret(pt){
+		if ('ReturnStatement' !== pt.type) return
+		return pt.argument
+	},
+	call(pt, cmp){
+		if ('CallExpression' !== pt.type) return
+		if (Array.isArray(cmp) && cmp[0] !== pt.arguments.length) return
+		return pt.callee
+	},
+	member(pt, cmp){
+		if ('MemberExpression' !== pt.type) return false
+		if (!Array.isArray(cmp) || 2 !== cmp.length) return true
+		if ('this' === cmp[0]){
+			return 'ThisExpression' === pt.object.type
+		}else{
+			return cmp[0] === pt.object.name
 		}
-		return false
-	})
-
-	return this
-}
-
-function AST(pt){
-	if (!(this instanceof AST)){
-		return new AST(pt)
+		return cmp[1] !== pt.property.name
 	}
-	this.pt = pt
-	return this
 }
 
-AST.find = find
+function find(steps, idx = 0){
+	if (steps.length <= idx || !this) return this
+	const [type, cmp] = steps[idx]
 
-AST.prototype = {
+	const pts = Array.isArray(this) ? this: [this]
+	for (let i=0, pt, found; pt = pts[i]; i++){
+		found = find.call(MAP_CMP[type](pt, cmp), steps, idx+1)
+		if (found) return found
+	}
+}
+
+return {
 	find,
-	reverse(){
-		this.pt = this.pt.reverse()
-		return this
-	}
 }
-
-return AST
--- module_builder.js
+++ module_builder.js
@@ -1,13 +1,33 @@
 const AST = require('~/ast')
 
+const NEXT = [['member', ['this', 'next']]]
 return {
 	add(mods, fname, ast){
-		const name = fname.split('.')[0]
+		const pts = AST.find.call(ast.body, [
+			['exp'],
+			['=', ['module', 'exports']],
+			['obj'],
+		])
+		console.log(pts)
+
 		const mod = {}
+		for(let i=0, pt, fbody, callee; pt = pts[i]; i++){
+			fbody = AST.find.call(pt, [
+				['prop'],
+				['func'],
+			])
+			console.log('*', i, fbody)
+			callee = AST.find.call(fbody, [
+				['ret'],
+				['call', [0]],
+			])
+			console.log('**', i, callee)
+			if (AST.find.call(callee, NEXT)){
+				mod[pt.key.name] = pt.value.params.map(p => p.name)
+			}
+		}
 
-		const pt = AST(ast).bfind('exp', AST.find('assign', ['module', 'exports']))
-		console.log(pt)
-
+		const name = fname.split('.')[0]
 		Object.assign(mods, {[name]: mod})
 		return mods
 	}
